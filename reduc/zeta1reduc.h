232e6bfe22e98220ad205e339e1f391febda9840
Borgar Lie <borgar.lie@gmail.com>
2017-02-21 23:32:52 +0100

    Add recursive doubling function

--- reduc/zeta1reduc.h
+++ reduc/zeta1reduc.h
@@ -33,9 +33,31 @@ double zeta1reduc(int iterations, int size, int rank) {
     return result;
 }
 
+// all reduce MPI function
+// double reduc(int size, int rank, double my_sum) {
+//     double sum = 0;
+//     MPI_Allreduce(&my_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+//     return sum;
+// }
+
+// my own implementation of all reduce (recursive doubling)
 double reduc(int size, int rank, double my_sum) {
-    double sum = 0;
-    MPI_Allreduce(&my_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    // initialise sum with the process own sum
+    double sum = my_sum;
+    int d = 1;
+    while (d < size) {
+        // exchange local accumulated sum with rank XOR d
+        int exchange = rank ^ d;
+        double recv = 0;
+        MPI_Sendrecv(&sum, 1, MPI_DOUBLE, exchange, 0, 
+                &recv, 1, MPI_DOUBLE, exchange, MPI_ANY_TAG, 
+                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        // add the received accumulated sum to local accumulated sum
+        sum += recv;
+        // left shift d by 1
+        d = d << 1;
+    }
+    // return the global sum
     return sum;
 }
 
