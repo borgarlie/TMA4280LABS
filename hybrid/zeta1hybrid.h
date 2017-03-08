c3ef350e50be02ecc0b2cfa9cdeec054bf561b95
Borgar Lie <borgar.lie@gmail.com>
2017-03-08 17:08:44 +0100

    some small error fixes

--- hybrid/zeta1hybrid.h
+++ hybrid/zeta1hybrid.h
@@ -2,9 +2,9 @@
 #define ZETA1_HYBRID_H
 
 double zeta1_hybrid(int n_threads_omp, int iterations, int size, int rank);
-void compute(int n, double *values);
+void compute(int n, double *values, int n_threads_omp);
 void scatter(double *local_values, double *values, int local_count);
-double calculate_my_sum(double *local_values, int count, int n_threads_omp);
+double calculate_my_sum(double *local_values, int count);
 
 double zeta1_hybrid(int n_threads_omp, int iterations, int size, int rank) {
 	// find local number of iterations
@@ -14,13 +14,13 @@ double zeta1_hybrid(int n_threads_omp, int iterations, int size, int rank) {
     if(rank == 0){
         // allocate enough space for the scatter operation (May this produce error if we do not set values to 0?)
         values = calloc((local_count * size) + size, sizeof(double));
-        compute(iterations, values); // &values ?
+        compute(iterations, values, n_threads_omp);
     }
     double *local_values = calloc(local_count, sizeof(double));
     // partition and send the data to other processors
     scatter(local_values, values, local_count);
     // calculate local sum and reduce them to rank 0
-    double my_sum = calculate_my_sum(local_values, local_count, n_threads_omp);
+    double my_sum = calculate_my_sum(local_values, local_count);
     double sum = 0;
     MPI_Reduce(&my_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     // free memory
@@ -33,7 +33,9 @@ double zeta1_hybrid(int n_threads_omp, int iterations, int size, int rank) {
     return result;
 }
 
-void compute(int n, double *values) {
+void compute(int n, double *values, int n_threads_omp) {
+    // calculate values using OpenMP
+    #pragma omp parallel for num_threads(n_threads_omp)
     for (int i=1; i<=n; i++) {
         values[i-1] = 1.0/((double)i*(double)i);
     }
@@ -44,10 +46,8 @@ void scatter(double *local_values, double *values, int local_count) {
     MPI_Scatter(values, local_count, MPI_DOUBLE, local_values, local_count, MPI_DOUBLE, 0, MPI_COMM_WORLD); 
 } 
 
-// calculate local sum using OpenMP
-double calculate_my_sum(double *local_values, int count, int n_threads_omp) {
+double calculate_my_sum(double *local_values, int count) {
 	double sum = 0;
-    #pragma omp parallel for num_threads(n_threads_omp) reduction(+:sum)
     for (int i=0; i<count; i++) {
         sum += local_values[i];
     }
--- zeta2/zeta2.h
+++ zeta2/zeta2.h
@@ -6,8 +6,8 @@ double zeta2 (int nthreads, int n) {
     double sum = 0;
 
     #pragma omp parallel for num_threads(nthreads) reduction(+:sum)
-    for (double i=1; i<=n; i++) {
-        sum += 1/(i*i);
+    for (int i=1; i<=n; i++) {
+        sum += 1.0/((double)i*(double)i);
     }
 
     double result = sqrt(sum*6);
